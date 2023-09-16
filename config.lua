7ad4e298c743ef7949442dbf396c795065c7d415
WarumDach <140314060+WarumDach@users.noreply.github.com>
2023-09-16 19:46:20 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,39 +1,50 @@
 Config = {}
 
+Config.Debug = false
+
+Config.DoorLocations = {
+    ['1'] = {name = 'Backstreet', location = vector3(2461.25, 1574.85, 33.11)},
+}
+
 
 Config.RobberyList = {
     [1] = {
         bank = true,
         Header = "Fleeca Banks",
         icon = "fa-solid fa-building-columns",
-        minCops = 4,
+        minCops = 0,
     },
     [2] = {
         bank = true,
         Header = "Paleto Bank",
         icon = "fa-solid fa-building-columns",
-        minCops = 4,
+        minCops = 0,
     },
     [3] = {
         bank = true,
         Header = "Pacific Bank",
         icon = "fa-solid fa-building-columns",
-        minCops = 4,
+        minCops = 0,
     },
-}
-
-Config.Peds = {
-    [1] = {
-        coords = vector4(979.37, -1816.61, 31.18, 281.08),
-        model = "s_m_y_blackops_01",
-    }
 }
 
 Config.Shop = {
     [1] = {
-        item = "phone",
-        price = 5450,
+        item = "electronickit",
+        price = 350,
         type = "cash",
         icon = "fa-solid fa-laptop-code",
+    },
+    [2] = {
+        item = "tunerlaptop",
+        price = 750,
+        type = "cash",
+        icon = "fa-solid fa-laptop-code",
+    },
+    [3] = {
+        item = "drill",
+        price = 150,
+        type = "cash",
+        icon = "fa-solid fa-screwdriver-wrench",
     }
 }
